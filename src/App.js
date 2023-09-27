8472d8f5453e3a6d899e3574a90947981ec9c6dc
Friedheilm <fridelok@gmail.com>
2023-09-27 14:33:24 +0200

    Adding loading function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ObjectList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      objects: [], // To store the fetched objects
+      isLoading: true, // To track loading state
+    };
+  }
+
+  componentDidMount() {
+    // Fetch data from a remote server (replace with your API endpoint)
+    fetch('https://api.example.com/objects')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          objects: data, // Store the fetched objects in state
+          isLoading: false, // Update loading state
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  render() {
+    const { objects, isLoading } = this.state;
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h1>List of Objects</h1>
+        <ul>
+          {objects.map((object) => (
+            <li key={object.id}>{object.name}</li>
+            // Replace 'id' and 'name' with the actual properties of your objects
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ObjectList;
